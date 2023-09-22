6503dcb50516b263a1ad0377c60bfff464c30dac
mtsdurica <mtsdurica@gmail.com>
2023-09-22 17:03:41 +0200

    feat: Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Example First Page</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="dist/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline">
+        Hello World!
+    </h1>
+    <a href="pages/second.php">Click me!</a>
+</body>
+
+</html>
---
+++ pages/second.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Example Second Page</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../dist/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline">
+        Second Page!
+    </h1>
+    <a href="../index.php">Go back!</a>
+</body>
+
+</html>
