f49e36176a93f158fc406ec591513857606775db
mtsdurica <mtsdurica@gmail.com>
2023-09-26 11:28:20 +0200

    feat: Add reply to DB

--- components/thread.php
+++ components/thread.php
@@ -11,3 +11,15 @@
 		</div>
 	</div>
 </div>
+<?php
+
+$replyQuery = $db->prepare('SELECT reply.reply_text, user.user_nick FROM reply LEFT JOIN user ON reply.poster_id = user.user_id WHERE reply.thread_id = ?');
+
+$replyQuery->execute([$threadId]);
+while($reply = $replyQuery->fetch(PDO::FETCH_ASSOC))
+{
+	$replyText  = $reply["reply_text"];
+	$replyPoster = $reply["user_nick"];
+	include "reply.php";
+}
+?>
--- index.php
+++ index.php
@@ -18,10 +18,8 @@ try {
     die();
 }
 
-$query = $db->prepare('SELECT thread.thread_title, thread.thread_text, reply.poster_id, reply.reply_text, U1.user_nick AS "thread_poster", U2.user_nick AS "reply_poster" FROM thread
-LEFT JOIN user U1 ON thread.poster_id = U1.user_id
-LEFT JOIN reply ON reply.thread_id = thread.thread_id
-LEFT JOIN user U2 ON reply.poster_id = U2.user_id');
+$query = $db->prepare('SELECT thread.thread_id, thread.thread_title, thread.thread_text, user.user_nick AS "thread_poster" FROM thread
+LEFT JOIN user ON thread.poster_id = user.user_id');
 
 $query->execute();
 
@@ -29,12 +27,8 @@ while ($thread = $query->fetch(PDO::FETCH_ASSOC)) {
     $threadTitle = $thread["thread_title"];
     $threadText = $thread["thread_text"];
     $threadPoster = $thread["thread_poster"];
-    $replyText = $thread["reply_text"];
-    $replyPosterId = $thread["poster_id"];
-    $replyPoster = $thread["reply_poster"];
+    $threadId = $thread["thread_id"];
     include "./components/thread.php";
-    if ($replyPosterId)
-        include "./components/reply.php";
 }
 ?>
 </body>
