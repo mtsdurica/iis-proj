e2806db1b562fd65bca6f14b6d2a6565ad5f658d
romanaduraciova <duraciovar@gmail.com>
2023-10-05 21:40:29 +0200

    error page design

--- views/error404View.php
+++ views/error404View.php
@@ -10,11 +10,29 @@
     <script type="text/javascript" src="./scripts/darkModeSetter.js"></script>
 </head>
 
-<body class="h-full">
-    <div class="flex flex-col h-full main-background-colorscheme">
-        
-        404 not found
+<?php
+$context = $_SERVER["CONTEXT_PREFIX"];
+session_start();
+?>
 
+<body class="items-center justify-center h-full main-background-colorscheme">
+    
+<!-- main container -->
+    <div class="flex flex-col items-center self-center justify-center h-full place-content-center text-colorscheme">
+       
+        <h1 class="text-2xl font-bold">
+            Oooops! Something went wrong.
+        </h1>  
+
+        <div class="m-5">
+            <img src="./images/404-error.png" alt="Page not found" style="width: 20rem;">
+        </div>  
+
+        <div class="flex flex-col items-center gap-1 px-5 py-2 text-2xl duration-300 rounded-lg cursor-pointer dark:hover:bg-slate-600 hover:bg-slate-200">
+            <a href="<?= $context ?>/"><i class="fa-solid fa-house "></i></a>
+            <p class="text-xs"> Back Home </p>
+        </div>
     </div>
+
     <script type="text/javascript" src="./scripts/main.js"></script>
 </body>
