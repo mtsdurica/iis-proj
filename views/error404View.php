74aa53bd6a860b442f707888687e29ae2f60def6
romanaduraciova <duraciovar@gmail.com>
2023-10-06 22:26:29 +0200

    added header to the page and little design changes

--- views/error404View.php
+++ views/error404View.php
@@ -8,6 +8,8 @@
     <link href="./dist/style.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/56e0bbdeed.js" crossorigin="anonymous"></script>
     <script type="text/javascript" src="./scripts/darkModeSetter.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js" integrity="sha256-xLD7nhI62fcsEZK2/v8LsBcb4lG7dgULkuXoXB/j91c=" crossorigin="anonymous"></script>
 </head>
 
 <?php
@@ -16,21 +18,30 @@ session_start();
 ?>
 
 <body class="items-center justify-center h-full main-background-colorscheme">
-    
-<!-- main container -->
+    <?php
+        require_once "./components/header.php";
+    ?>
+    <!-- main container -->
     <div class="flex flex-col items-center self-center justify-center h-full place-content-center text-colorscheme">
        
+        <div class="m-0">
+            <img src="./images/404-error.png" alt="Page not found" style="width: 20rem;">
+        </div> 
+    
         <h1 class="text-2xl font-bold">
             Oooops! Something went wrong.
         </h1>  
 
-        <div class="m-5">
-            <img src="./images/404-error.png" alt="Page not found" style="width: 20rem;">
-        </div>  
+        <p class="mt-3">
+            The page you are looking for was moved, removed, renamed or does not exist.
+        </p>
+ 
 
-        <div class="flex flex-col items-center gap-1 px-5 py-2 text-2xl duration-300 rounded-lg cursor-pointer dark:hover:bg-slate-600 hover:bg-slate-200">
-            <a href="<?= $context ?>/"><i class="fa-solid fa-house "></i></a>
-            <p class="text-xs"> Back Home </p>
+        <div class="flex flex-row items-center justify-center px-5 py-2 mt-20 text-2xl bg-red-500 rounded-full bg-opacity-70 hover:bg-red-800">
+            <a href="<?= $context ?>/" class="flex items-center justify-center gap-2 px-2 py-1 flew-row"> 
+                <i class="fa-solid fa-chevron-left"></i>
+                <span class="text-sm"> Back to Home </span> 
+            </a>
         </div>
     </div>
 
