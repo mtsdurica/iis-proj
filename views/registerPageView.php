99a27ee5094baf4c916176a355dc8e0acd63a613
romanaduraciova <duraciovar@gmail.com>
2023-10-12 23:09:56 +0200

    initial file for register page view

---
+++ views/registerPageView.php
@@ -0,0 +1,30 @@
+<?php
+$context = $_SERVER["CONTEXT_PREFIX"];
+session_start();
+?>
+
+<!DOCTYPE html>
+<html class="h-full">
+
+<head>
+    <title>Register</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Tailwind -->
+    <link href="<?= $context ?>/dist/style.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/56e0bbdeed.js" crossorigin="anonymous"></script>
+    <!-- Dark mode -->
+    <script type="text/javascript" src="<?= $context ?>/scripts/darkModeSetter.js"></script>
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js" integrity="sha256-xLD7nhI62fcsEZK2/v8LsBcb4lG7dgULkuXoXB/j91c=" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    
+</body>
+    hello
+</html>
+
