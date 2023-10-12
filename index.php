7b4764ddd415a7ae1885af37737a19483a9bb022
romanaduraciova <duraciovar@gmail.com>
2023-10-12 23:08:46 +0200

    added register page to router

--- index.php
+++ index.php
@@ -22,6 +22,9 @@ switch ($exploded[2]) {
     case ('logout'):
         require_once __DIR__ . "/scripts/logout.php";
         break;
+    case ('register'):
+        require_once __DIR__ . "/views/registerPageView.php";
+        break;
     default:
         http_response_code(404);
         require_once __DIR__ . "/views/error404View.php";
