19bc49b372f8eecc5af020c50b76b6de80e867ec
romanaduraciova <duraciovar@gmail.com>
2023-10-12 21:41:14 +0200

    fix: issue problems solved
    removed scrolling, changed html head, added transition to the Back To Home button

--- views/error404View.php
+++ views/error404View.php
@@ -5,7 +5,7 @@
     <title>ERROR 404</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./dist/style.css" rel="stylesheet">
+    <link href="<?= $context ?>/dist/style.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/56e0bbdeed.js" crossorigin="anonymous"></script>
     <script type="text/javascript" src="./scripts/darkModeSetter.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
@@ -17,12 +17,12 @@ $context = $_SERVER["CONTEXT_PREFIX"];
 session_start();
 ?>
 
-<body class="items-center justify-center h-full main-background-colorscheme">
+<body class="items-center justify-center h-full overflow-hidden main-background-colorscheme">
     <?php
         require_once "./components/header.php";
     ?>
     <!-- main container -->
-    <div class="flex flex-col items-center self-center justify-center h-full place-content-center text-colorscheme">
+    <div class="flex flex-col items-center justify-center h-full text-colorscheme">
        
         <div class="m-0">
             <img src="./images/404-error.png" alt="Page not found" style="width: 20rem;">
@@ -35,14 +35,14 @@ session_start();
         <p class="mt-3">
             The page you are looking for was moved, removed, renamed or does not exist.
         </p>
- 
 
-        <div class="flex flex-row items-center justify-center px-5 py-2 mt-20 text-2xl bg-red-500 rounded-full bg-opacity-70 hover:bg-red-800">
-            <a href="<?= $context ?>/" class="flex items-center justify-center gap-2 px-2 py-1 flew-row"> 
+        <a href="<?= $context ?>/" class="flex flex-row items-center justify-center px-5 py-2 mt-20 text-2xl transition-all duration-300 bg-red-500 rounded-full bg-opacity-70 hover:bg-red-800">
+            <span class="flex items-center justify-center gap-2 px-2 py-1 flew-row"> 
                 <i class="fa-solid fa-chevron-left"></i>
                 <span class="text-sm"> Back to Home </span> 
-            </a>
-        </div>
+            </span>
+        </a>
+    
     </div>
 
     <script type="text/javascript" src="./scripts/main.js"></script>
