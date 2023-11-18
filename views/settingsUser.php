07ad5186a903386627f3ff666ffcf20222dd58c4
romanaduraciova <duraciovar@gmail.com>
2023-11-18 21:47:24 +0100

    feat: upload profile pic to server

--- views/settingsUser.php
+++ views/settingsUser.php
@@ -13,8 +13,6 @@ $userFullname = $userData["user_full_name"];
 $userEmail = $userData["user_email"];
 $userGender = $userData["user_gender"];
 $userBirthdate = $userData["user_birthdate"];
-
-echo "user settings";
 ?>
 
 <!DOCTYPE html>
@@ -30,3 +28,49 @@ echo "user settings";
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js" integrity="sha256-xLD7nhI62fcsEZK2/v8LsBcb4lG7dgULkuXoXB/j91c=" crossorigin="anonymous"></script>
 </head>
+
+<body class="items-center h-full main-background-colorscheme text-colorscheme">
+    <!-- Page Container -->
+    <div class="flex flex-col h-full">
+        <?php
+        require_once "./components/header.php";
+        ?>
+        <!-- Page Content Container -->
+        <div class="flex flex-row items-center justify-center h-full">
+            <!-- Left side -->
+            <div class="flex flex-col">
+                <!-- Profile picture section -->
+                <div class="flex flex-col gap-2">
+                    <h1 class="font-bold text-2xl my-3">Profile picture</h1>
+                    <!-- TODO: condition that will upload photo from database -->
+                    <img src="<?= $context ?>/images/profile_photo.jpg" alt="Profile picture" width="150rem" height="150rem" class="transition-all rounded-lg m-2">
+                    
+                    <form class="flex flex-row justify-center text-center" action="<?= $context ?>/scripts/uploadProfilePic.php" method="post" enctype="multipart/form-data">
+                        <input type="file" name="fileToUpload" id="fileToUpload">
+                        <input class="px-2 py-1 text-sm text-center text-white transition-all rounded-lg basic-button-colorscheme" type="submit" name="submit" value="Upload image">
+                    </form>
+                </div> <!-- Profile picture section -->
+
+                <!-- General settings form section -->
+                <div class="flex flex-col">
+                    <h2>General</h2>
+                </div> <!-- General settings form section -->
+
+            </div> <!-- Left side -->
+
+            <!-- Right side -->
+            <div class="flex flex-col">
+                <!-- Banner picture section -->
+                <div>
+                    <h2>Banner</h2>
+                </div> <!-- Banner picture section -->
+
+                <!-- Password settings form section -->
+                <div>
+                    <h2>Password</h2>
+                </div> <!-- Password settings form section -->
+
+            </div> <!-- Right side -->
+        </div>
+    </div>
+</body>
